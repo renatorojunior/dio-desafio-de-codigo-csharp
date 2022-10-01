b31bb5edf2d3526c5382ac2966a2b311e0ef060c
Renato Rodrigues <65936388+renatorojunior@users.noreply.github.com>
2022-10-01 12:01:11 -0300

    Resolvendo Desafio 01.

---
+++ 01SubtraindoProdutoEaSomaDosDigitosDeUmNumero/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class DIO
+{
+
+    static void Main(string[] args)
+    {
+        //Desenvolvendo aplicação para subtrair o produto e a soma de um número inteiro.
+        int n, ni, produto, soma, diferença;
+
+        Console.WriteLine("Digite o número que deseja calcular a diferença entre o produto e a soma:");
+        n = int.Parse(Console.ReadLine());
+        ni = n;
+        produto = 1;
+        soma = 0;
+        
+
+        while (ni > 0)
+        {
+            int l = ni % 10;
+            
+            produto *= l;
+            soma += l;
+            ni /= 10;
+        }
+
+        diferença = produto - soma;
+
+        Console.WriteLine($"A diferença entre a soma e o produto dos digitos do número inteiro {n} é {diferença}.");
+    }
+}
