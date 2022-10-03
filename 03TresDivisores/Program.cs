8ed687a71dfcbdaa2b74524281a599b7f1494909
Renato Rodrigues <65936388+renatorojunior@users.noreply.github.com>
2022-10-02 22:54:41 -0300

    Resolvendo questão 03.

---
+++ 03TresDivisores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class DIO
+{
+        
+    public static void Main(string[] args)
+    {
+        // Implementando solução para verificar se um número n tem exatamente três divisores positivos.
+        Console.WriteLine("Qual número deseja verificar?");
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                count++;
+            }
+            if (count > 3)
+            {
+                Console.WriteLine(false);
+            }
+        }
+
+        if(count == 3)
+        {
+            Console.WriteLine($"O número {n} tem exatamente 3 divisores.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {n} não tem exatamente 3 divisores.");
+        }
+    }
+}
