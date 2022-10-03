2c0cf8c2f3d4d9c41af846a29532bac17a29f353
Renato Rodrigues <65936388+renatorojunior@users.noreply.github.com>
2022-10-02 22:36:06 -0300

    Resolvendo desafio 02

---
+++ 02EncaixaOuNao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class Dio
+{
+
+    static void Main(string[] args)
+    {
+        // Implementando solução para verificar se o valor B corresponde aos últimos dígitos de A.
+        Console.WriteLine("Quantos casos serão analisados?");      
+        int n = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Digite o {i+1}º caso:");
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+
+            if (a.Length < b.Length)
+                Console.WriteLine("Não encaixa.");
+            else if (a.EndsWith(b))
+                Console.WriteLine("Encaixa.");
+            else
+                Console.WriteLine("Não encaixa.");
+
+        }
+
+    }
+
+}
